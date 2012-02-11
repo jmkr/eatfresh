4e9cd33477b6fc6510d975b58685b408ce67676b
David Schnurr <schnurr@gmail.com>
2012-02-11 11:40:39 -0800

    added eval stuffs

--- notJS/interpreter.scala
+++ notJS/interpreter.scala
@@ -180,15 +180,188 @@ object Interp {
   // the global Store
   val gStore = Store()
 
-  // the evaluation function [[.]] \in Config -> Value
-  def eval(config:Config): Value = {
-    // FILL ME IN
-    def evalTo(t:Term): Value = t match {
-      case _ => UnitV()
-    }
+// the evaluation function [[.]] \in Config -> Value
+def eval(config:Config): Value = {
+	val env = config.env
 
-    evalTo(config.t)
-  }
+	// since we'll be making lots of recursive calls where the
+	// environment doesn't change, we'll define an inner function that
+	// will leave env as a free variable
+	def evalTo(t:Term): Value = t match {
+		case Seq(t1, t2) => {
+			evalTo(t1)
+			evalTo(t2)
+		}
+		case Assign(x, e) => {
+			gStore(env(x)) = evalTo(e)
+			UnitV()
+		}
+		case w @ While(e, t) => evalTo(e) match {
+			case BoolV(true) => {
+				evalTo(t)
+				evalTo(w)
+			}
+			case BoolV(false) => UnitV()
+			case _ => throw undefined("while guard not a bool")
+		}
+		case Out(e) => {
+			/*def val2str(v:Value): String = v match {
+				case a:Address => "{" + list2str(a) + "}"
+				case StringV(s) if s == "" => "\"\""
+				case other => other.toString
+			}
+			def obj2str(a:Address): String = gStore(a) match {
+				case Empty() => ""
+				case Object(m) => val2str(v) + (obj2str(a) match {
+					case "" => ""
+					case s  => ", " + s
+				})
+				case _ => throw undefined("outputting illegal value")
+			}
+			println(val2str(evalTo(e)))*/
+			println(evalTo(e))
+			UnitV()
+		}
+		case Update(e1, e2, e3) => (evalTo(e1), evalTo(e2), evalTo(e3)) match {
+			//Fill 
+			case(a:Address, s:StringV, v:Value) => UnitV()
+			case _ => UnitV()
+		}
+		/*case HAssign(e1, e2) => (evalTo(e1), evalTo(e2)) match {
+			case (a:Address, v:Value) => gStore(a) match {
+				case ListF(vH, aT) => {
+					gStore(a) = ListF(v, aT)
+					UnitV()
+				}
+				case _ => throw undefined("assigning to head of empty list")
+			}
+			case _ => throw undefined("assigning to head of non-list")
+		}
+		case TAssign(e1, e2) => (evalTo(e1), evalTo(e2)) match {
+			case (a1:Address, a2:Address) => (gStore(a1), gStore(a2)) match {
+				case (ListF(vH, aT), _:ListV) => {
+					gStore(a1) = ListF(vH, a2)
+					UnitV()
+				}
+				case _ => throw undefined("assigning to tail of empty list")
+			}
+			case _ => throw undefined("bad tail assignment")
+		}*/
+		case Num(n) => NumV(n)
+		case Bool(b) => BoolV(b)
+		case Str(str) => StringV(str)
+		case NotUnit() => UnitV()
+		case x:Var => gStore(env(x))
+		case Not(e) => evalTo(e) match {
+			case BoolV(b) => BoolV(!b)
+			case _ => throw undefined("negated expression not a bool")
+		}
+		case BinOp(bop, e1, e2) => bop match {
+			case Equal => {
+				val v1 = evalTo(e1) match {
+					case a:Address => gStore(a)
+					case v => v
+				}
+				val v2 = evalTo(e2) match {
+					case a:Address => gStore(a)
+					case v => v
+				}
+				BoolV(v1 == v2)
+			}
+			case _ => (evalTo(e1), evalTo(e2)) match {
+				case (BoolV(b1), BoolV(b2)) => bop match {
+					case And => BoolV(b1 && b2)
+					case Or  => BoolV(b1 || b2)
+					case _   => throw undefined("illegal operation on bools")
+				}
+				case (NumV(n1), NumV(n2)) => bop match {
+					case Add => NumV(n1 + n2)
+					case Sub => NumV(n1 - n2)
+					case Mul => NumV(n1 * n2)
+					case Div if n2 != 0 => NumV(n1 / n2)
+					case Lte => BoolV(n1 <= n2)
+					case _   => throw undefined("illegal operation on nums")
+				}
+				case (StringV(s1), StringV(s2)) => bop match {
+					case Add => StringV(s1 + s2)
+					case Lte => BoolV(s1 <= s2)
+					case _   => throw undefined("illegal operation on strings")
+				}
+				/*case (v:Value, a:Address) => bop match {
+					case Cons => gStore(a) match {
+						case _:ListV => alloc(ListF(v, a))
+						case _ => throw undefined("illegal value for cons")
+					}
+					case _ => throw undefined("illegal operation on lists")
+				}*/
+				case _ => throw undefined("illegal binary operation")
+			}
+		}
+		case If(e, t1, t2) => evalTo(e) match {
+			case BoolV(true)  => evalTo(t1)
+			case BoolV(false) => evalTo(t2)
+			case _ => throw undefined("if guard not a bool")
+		}
+		case In(typ) => typ match {
+			case NumT => NumV(BigInt(scala.Console.readLine()))
+			case StrT => StringV(scala.Console.readLine())
+		}
+		case Call(ef, es) => evalTo(ef) match {
+			case clo @ FunClo(cloEnv, Fun(f, xs, t)) => {
+				if (xs.length != es.length) 
+					throw undefined("arguments and parameters don't match")
+
+				val xv = (f :: xs) zip (clo :: (es map evalTo))
+				val newEnv = xv.foldLeft( cloEnv )(
+					(env, xv) => env + (xv._1 -> alloc(xv._2)))
+					eval(Config(t, newEnv))
+			}
+			case _ => throw undefined("calling a non-closure")
+		}
+		/*case NotList(es) => makeList(es map evalTo)
+		case Head(e) => evalTo(e) match {
+			case a:Address => gStore(a) match {
+				case ListF(v, _) => v
+				case _ => throw undefined("taking the head of an empty list")
+			}
+			case _ => throw undefined("taking the head of a non-list")
+		}
+		case Tail(e) => evalTo(e) match {
+			case a:Address => gStore(a) match {
+				case ListF(_, aT) => aT
+				case _ => throw undefined("taking the tail of an empty list")
+			}
+			case _ => throw undefined("taking the tail of a non-list")
+		}*/
+		case Obj(fbs) => {
+			// FILL ME IN
+			UnitV()
+		}
+		case Access(e1, e2) => {
+			// FILL ME IN
+			UnitV()
+		}
+		case Method(xs, t) => {
+			// FILL ME IN
+			UnitV()
+		}
+		case MCall(eO, eF, es) => {
+			// FILL ME IN
+			UnitV()
+		}
+		case Block(vbs, t) => {
+			val dummies = for ( VarBind(x,_) <- vbs ) yield (x, UnitV())
+			val newEnv = dummies.foldLeft( env )(
+				(env, xv) => env + (xv._1 -> alloc(xv._2)) )
+        
+				for ( VarBind(x, e) <- vbs ) gStore(newEnv(x)) = eval(Config(e, newEnv))
+				eval(Config(t, newEnv))
+			}
+			case f:Fun => FunClo(env, f)
+		}
+
+		evalTo(config.t)
+	}
 }
 
 } // end package cs162.notJS.interpreter
